6e13c0a7bfb0e392aa0be63cdb5e2a531f4b5d77
jeyaAndRohini <jeya.4321@gmail.com>
2015-11-28 18:52:43 +0530

    The Basic UI template for home page

--- child-pal/src/main/java/com/surabhi/config/Config.java
+++ child-pal/src/main/java/com/surabhi/config/Config.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -12,16 +13,17 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @ComponentScan(basePackages = "com.surabhi")
 @EnableWebMvc
 public class Config extends WebMvcConfigurerAdapter {
-	/*@Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-    }*/
-	
+	@Override
+	public void configureDefaultServletHandling(
+			DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
 	@Bean
-    public ViewResolver getViewResolver() {
-        InternalResourceViewResolver bean = new InternalResourceViewResolver();
-        bean.setPrefix("/WEB-INF/view/");
-        bean.setSuffix(".jsp");
-        return bean;
-    }
+	public ViewResolver getViewResolver() {
+		InternalResourceViewResolver bean = new InternalResourceViewResolver();
+		bean.setPrefix("/WEB-INF/view/");
+		bean.setSuffix(".jsp");
+		return bean;
+	}
 }
--- child-pal/src/main/java/com/surabhi/controller/SurabhiController.java
+++ child-pal/src/main/java/com/surabhi/controller/SurabhiController.java
@@ -1,6 +1,5 @@
 package com.surabhi.controller;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,11 +7,17 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SurabhiController {
-	
-	@RequestMapping(value= "/home",method=RequestMethod.GET)
+
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public ModelAndView getIndexPage() {
 		ModelAndView view = new ModelAndView("homePage");
 		return view;
 	}
 
+	/*
+	 * @RequestMapping(value = "/home", method = RequestMethod.GET) public
+	 * ModelAndView getI() {
+	 * 
+	 * return getIndexPage(); }
+	 */
 }
