7bc27fa1c43871c109909e0cdf01419030f22705
Dilnawaz B S <dilnawaz.bs@tcs.com>
2015-11-28 17:22:12 +0100

    committing pal domain class

---
+++ child-pal/src/main/java/com/surabhi/domain/Pal.java
@@ -0,0 +1,345 @@
+package com.surabhi.domain;
+
+import java.util.Date;
+
+public class Pal {
+	private String name;
+	private String classStudying;
+	private String medium;
+	private String schoolName;
+	private String place;
+	private String district;
+	private String pin;
+	private Date dob;
+	private Date sex;
+	private String subject1;
+	private String subject2;
+	private String sports1;
+	private String sports2;
+	private String interest;
+	private String dream;
+	private String fatherName;
+	private String motherName;
+	private String hero;
+	private String address;
+	private int phone;
+	private String sportsSpecialisation;
+	private int schoolContact;
+	private int userId;
+	private int id;
+	private int mentorId;
+	private int beneficiaryid;
+	/**
+	 * @return the beneficiaryid
+	 */
+	public int getBeneficiaryid() {
+		return beneficiaryid;
+	}
+	/**
+	 * @param beneficiaryid the beneficiaryid to set
+	 */
+	public void setBeneficiaryid(int beneficiaryid) {
+		this.beneficiaryid = beneficiaryid;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the classStudying
+	 */
+	public String getClassStudying() {
+		return classStudying;
+	}
+	/**
+	 * @param classStudying the classStudying to set
+	 */
+	public void setClassStudying(String classStudying) {
+		this.classStudying = classStudying;
+	}
+	/**
+	 * @return the medium
+	 */
+	public String getMedium() {
+		return medium;
+	}
+	/**
+	 * @param medium the medium to set
+	 */
+	public void setMedium(String medium) {
+		this.medium = medium;
+	}
+	/**
+	 * @return the schoolName
+	 */
+	public String getSchoolName() {
+		return schoolName;
+	}
+	/**
+	 * @param schoolName the schoolName to set
+	 */
+	public void setSchoolName(String schoolName) {
+		this.schoolName = schoolName;
+	}
+	/**
+	 * @return the place
+	 */
+	public String getPlace() {
+		return place;
+	}
+	/**
+	 * @param place the place to set
+	 */
+	public void setPlace(String place) {
+		this.place = place;
+	}
+	/**
+	 * @return the district
+	 */
+	public String getDistrict() {
+		return district;
+	}
+	/**
+	 * @param district the district to set
+	 */
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+	/**
+	 * @return the pin
+	 */
+	public String getPin() {
+		return pin;
+	}
+	/**
+	 * @param pin the pin to set
+	 */
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+	/**
+	 * @return the dob
+	 */
+	public Date getDob() {
+		return dob;
+	}
+	/**
+	 * @param dob the dob to set
+	 */
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	/**
+	 * @return the sex
+	 */
+	public Date getSex() {
+		return sex;
+	}
+	/**
+	 * @param sex the sex to set
+	 */
+	public void setSex(Date sex) {
+		this.sex = sex;
+	}
+	/**
+	 * @return the subject1
+	 */
+	public String getSubject1() {
+		return subject1;
+	}
+	/**
+	 * @param subject1 the subject1 to set
+	 */
+	public void setSubject1(String subject1) {
+		this.subject1 = subject1;
+	}
+	/**
+	 * @return the subject2
+	 */
+	public String getSubject2() {
+		return subject2;
+	}
+	/**
+	 * @param subject2 the subject2 to set
+	 */
+	public void setSubject2(String subject2) {
+		this.subject2 = subject2;
+	}
+	/**
+	 * @return the sports1
+	 */
+	public String getSports1() {
+		return sports1;
+	}
+	/**
+	 * @param sports1 the sports1 to set
+	 */
+	public void setSports1(String sports1) {
+		this.sports1 = sports1;
+	}
+	/**
+	 * @return the sports2
+	 */
+	public String getSports2() {
+		return sports2;
+	}
+	/**
+	 * @param sports2 the sports2 to set
+	 */
+	public void setSports2(String sports2) {
+		this.sports2 = sports2;
+	}
+	/**
+	 * @return the interest
+	 */
+	public String getInterest() {
+		return interest;
+	}
+	/**
+	 * @param interest the interest to set
+	 */
+	public void setInterest(String interest) {
+		this.interest = interest;
+	}
+	/**
+	 * @return the dream
+	 */
+	public String getDream() {
+		return dream;
+	}
+	/**
+	 * @param dream the dream to set
+	 */
+	public void setDream(String dream) {
+		this.dream = dream;
+	}
+	/**
+	 * @return the fatherName
+	 */
+	public String getFatherName() {
+		return fatherName;
+	}
+	/**
+	 * @param fatherName the fatherName to set
+	 */
+	public void setFatherName(String fatherName) {
+		this.fatherName = fatherName;
+	}
+	/**
+	 * @return the motherName
+	 */
+	public String getMotherName() {
+		return motherName;
+	}
+	/**
+	 * @param motherName the motherName to set
+	 */
+	public void setMotherName(String motherName) {
+		this.motherName = motherName;
+	}
+	/**
+	 * @return the hero
+	 */
+	public String getHero() {
+		return hero;
+	}
+	/**
+	 * @param hero the hero to set
+	 */
+	public void setHero(String hero) {
+		this.hero = hero;
+	}
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	/**
+	 * @return the phone
+	 */
+	public int getPhone() {
+		return phone;
+	}
+	/**
+	 * @param phone the phone to set
+	 */
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	/**
+	 * @return the sportsSpecialisation
+	 */
+	public String getSportsSpecialisation() {
+		return sportsSpecialisation;
+	}
+	/**
+	 * @param sportsSpecialisation the sportsSpecialisation to set
+	 */
+	public void setSportsSpecialisation(String sportsSpecialisation) {
+		this.sportsSpecialisation = sportsSpecialisation;
+	}
+	/**
+	 * @return the schoolContact
+	 */
+	public int getSchoolContact() {
+		return schoolContact;
+	}
+	/**
+	 * @param schoolContact the schoolContact to set
+	 */
+	public void setSchoolContact(int schoolContact) {
+		this.schoolContact = schoolContact;
+	}
+	/**
+	 * @return the userId
+	 */
+	public int getUserId() {
+		return userId;
+	}
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the mentorId
+	 */
+	public int getMentorId() {
+		return mentorId;
+	}
+	/**
+	 * @param mentorId the mentorId to set
+	 */
+	public void setMentorId(int mentorId) {
+		this.mentorId = mentorId;
+	}
+	
+}
