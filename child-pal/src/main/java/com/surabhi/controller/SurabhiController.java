012dd86356c7351c5740d5f68bad6ee26b6e94c9
Dilnawaz B S <dilnawaz.bs@tcs.com>
2015-11-29 02:37:43 +0100

    Pal finding logic implemented

--- child-pal/src/main/java/com/surabhi/controller/SurabhiController.java
+++ child-pal/src/main/java/com/surabhi/controller/SurabhiController.java
@@ -1,20 +1,19 @@
 package com.surabhi.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.surabhi.domain.Pal;
-
+import com.surabhi.service.ExactMatchGenerator;
 @Controller
 public class SurabhiController {
+	@Autowired
+	ExactMatchGenerator exactMatchGenerator;
 
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public ModelAndView getIndexPage() {
@@ -22,39 +21,20 @@ public class SurabhiController {
 		return view;
 	}
 
-	@RequestMapping(value = "/addPal", method = RequestMethod.POST)
+	@RequestMapping(value = "/addPal", method = RequestMethod.GET)
 	public ModelAndView addPal(@ModelAttribute Pal pal, ModelMap model) {
-
-		// data received from PAL
-		System.out.println("username: " + pal.getName());
+		exactMatchGenerator.generateExactmatch(pal);
+		model.addAttribute("name", pal.getName());
+		model.addAttribute("gender", pal.getGender());
+		/*System.out.println("username: " + pal.getName());
 		System.out.println("standard: " + pal.getStandard());
 		System.out.println("gender: " + pal.getGender());
 		System.out.println("subject: " + pal.getLikingSubject());
 		System.out.println("sport: " + pal.getLikingSport());
-		System.out.println("other interest: " + pal.getOtherInterest());
-
-		// List of beneficiaries retrieved from db
-		List<Pal> benefeciaries = new ArrayList<>();
-		Pal pal1 = new Pal();
-		Pal pal2 = new Pal();
-		Pal pal3 = new Pal();
-		pal1.setName("qwe");
-		pal2.setName("iio");
-		pal3.setName("sjdh");
-		benefeciaries.add(pal1);
-		benefeciaries.add(pal2);
-		benefeciaries.add(pal3);
-		model.addAttribute("benefeciaries", benefeciaries);
-
+		System.out.println("other interest: " + pal.getOtherInterest());*/
 		return new ModelAndView("displayBeneficiery");
 	}
-
-	@RequestMapping(value = "/connectBeneficiary", method = RequestMethod.POST)
-	public ModelAndView connectBeneficiary(
-			@RequestParam String beneficiaryIndex, ModelMap model) {
-		System.out.println("beneficiaryIndex: " + beneficiaryIndex);
-		return new ModelAndView("ThankYou");
-	}
+	
 	/*
 	 * @RequestMapping(value = "/home", method = RequestMethod.GET) public
 	 * ModelAndView getI() {
